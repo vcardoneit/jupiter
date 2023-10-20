5cf115482c9c23dd3c58ad7acadf2f76680012d0
Vincenzo Cardone <info@vcardone.it>
2023-10-20 10:30:35 +0200

    fix: client_max_body_size

--- idoneita/urls.py
+++ idoneita/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('idoneita', views.idoneita, name="idoneita"),
+    path('idoneita/', views.idoneita, name="idoneita"),
     path('idoneita/modifica/', views.modifica, name="modifica_predonazione"),
     path('idoneita/aggiungi/', views.aggiungi, name="aggiungi_predonazione"),
     path('idoneita/salva/', views.salva, name="salva_predonazione"),
